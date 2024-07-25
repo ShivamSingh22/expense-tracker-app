898927467ff23ace9f70b2b3256dadf0e59127a5
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-07-25 15:28:26 +0530

    Signup api done

--- public/index.js
+++ public/index.js
@@ -1,20 +1,26 @@
-function handleFormSubmit(event){
+function handleFormSubmit(event) {
     event.preventDefault();
 
     const username = event.target.username.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
 
-    const obj ={
+    const messageTxt = document.getElementById('msg-txt');
+
+    const obj = {
         username: username,
         email: email,
         password: password
-    }
+    };
 
-    axios
-    .post('http://localhost:3000/user/signup',obj)
-    .then((res)=> {
-        event.target.reset();
-    })
-    .catch(err=> console.log(err));
+    axios.post('http://localhost:3000/user/signup', obj)
+        .then((res) => {
+      
+            messageTxt.innerHTML = res.data.message;
+            event.target.reset();
+        })
+        .catch(err => {
+            messageTxt.innerHTML = err.response.data.message;
+          console.log(err);
+        });
 }
