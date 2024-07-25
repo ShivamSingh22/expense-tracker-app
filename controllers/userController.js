8d16e05158878d38640ca638f4765cc5033bb9ef
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-07-25 16:18:52 +0530

    Login Functionality Done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,43 @@
-const express = require('express');
-const Signup = require('../models/signup');
+const express = require("express");
+const Signup = require("../models/signupModel");
 
-exports.postSignup = async (req,res,next) => {
-    const {username} = req.body;
-    const {email} = req.body;
-    const {password} = req.body;
-    try{
-        const existingUser = await Signup.findOne({where: {email}});
+exports.postSignup = async (req, res, next) => {
+  const { username } = req.body;
+  const { email } = req.body;
+  const { password } = req.body;
+  try {
+    const existingUser = await Signup.findOne({ where: { email } });
 
-        if(existingUser){
-            return res.status(403).json({message: "ERROR: USER ALREADY EXISTS"});
-        }else{
-            await Signup.create({
-                username:username,
-                email:email,
-                password:password
-            });
-            res.status(201).json({message: "Signup Completed!!"});
-        }
-        
-    }catch(error){
-        res.status(500).json({ error: error.message });
+    if (existingUser) {
+      return res.status(403).json({ message: "ERROR: USER ALREADY EXISTS" });
+    } else {
+      await Signup.create({
+        username: username,
+        email: email,
+        password: password,
+      });
+      res.status(201).json({ message: "Signup Completed!!" });
     }
-    
-}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.postLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Signup.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(403).json({ message: "USER DOES NOT EXIST!!" });
+    }
+    if (user.password !== password) {
+      return res.status(403).json({ message: "BAD CREDENTIALS!!" });
+    }
+
+    res.status(200).json({ message: "Login successful!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ public/login.js
@@ -0,0 +1,23 @@
+function handleFormSubmit(event){
+    event.preventDefault();
+    
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const messageLabel = document.querySelector('#msg-txt');
+
+    const obj = {
+        email:email,
+        password:password
+    }
+
+    axios.post('http://localhost:3000/user/login',obj)
+    .then((res)=>{
+        console.log(res);
+        messageLabel.innerHTML = res.data.message;
+        event.target.reset();
+    })
+    .catch(err=>{
+        messageLabel.innerHTML = err.response.data.message;
+        console.log(err);
+    })
+}
