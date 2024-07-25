8f1f88c06e2f808c744163845a1f24756207f7b1
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-07-25 15:28:26 +0530

    Signup api done

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,12 +6,19 @@ exports.postSignup = async (req,res,next) => {
     const {email} = req.body;
     const {password} = req.body;
     try{
-        await Signup.create({
-            username:username,
-            email:email,
-            password:password
-        });
-        res.status(201).json({message: "Signup Completed!!"});
+        const existingUser = await Signup.findOne({where: {email}});
+
+        if(existingUser){
+            return res.status(403).json({message: "ERROR: USER ALREADY EXISTS"});
+        }else{
+            await Signup.create({
+                username:username,
+                email:email,
+                password:password
+            });
+            res.status(201).json({message: "Signup Completed!!"});
+        }
+        
     }catch(error){
         res.status(500).json({ error: error.message });
     }
