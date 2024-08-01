7f47d2102dc2ec04c725fd6047481130b1ad3389
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-08-01 16:53:35 +0530

    Razorpay Integration

--- public/expense/expense.js
+++ public/expense/expense.js
@@ -1,3 +1,5 @@
+// const Razorpay = require('razorpay');
+
 window.addEventListener('DOMContentLoaded',()=>{
     const token = localStorage.getItem('token');
     axios.get('http://localhost:3000/expense/all',{ headers : {"Authorization" : token}})
@@ -9,6 +11,52 @@ window.addEventListener('DOMContentLoaded',()=>{
     })
     .catch(err => console.log(err));
 })
+document.getElementById('rzp-button1').onclick = async function(e) {
+    const token = localStorage.getItem('token');
+    try {
+        const response = await axios.get('http://localhost:3000/purchase/premiummembership', { headers: { "Authorization": token } });
+        console.log(response);
+
+        var options = {
+            "key": response.data.key_id,
+            "order_id": response.data.order.id,
+            "handler": async function(response) {
+                try {
+                    await axios.post('http://localhost:3000/purchase/updatetransactionstatus', {
+                        order_id: options.order_id,
+                        payment_id: response.razorpay_payment_id,
+                    }, {
+                        headers: { "Authorization": token }
+                    });
+                    alert('YOU ARE A PREMIUM USER NOW');
+                } catch (err) {
+                    console.error(err);
+                }
+            },
+        };
+
+        const rzp1 = new Razorpay(options);
+        rzp1.open();
+        e.preventDefault();
+
+        rzp1.on('payment.failed', async function(response) {
+            console.log(response);
+            alert('Something went wrong');
+            try {
+                await axios.post('http://localhost:3000/purchase/transactionfailed', {
+                    order_id: options.order_id  // Use order_id instead of orderid
+                }, {
+                    headers: { "Authorization": token }
+                });
+            } catch (error) {
+                console.log('ERROR IN PAYMENT FAILED UPDATION: ' + error);
+            }
+        });
+    } catch (error) {
+        console.error('Error during premium membership request:', error);
+    }
+};
+
 
 function displayExpenses(expenseDetail){
     const ul = document.getElementById('expenseList');
@@ -26,7 +74,7 @@ function displayExpenses(expenseDetail){
 
 function deleteExpense(expId,listItem){
     const token = localStorage.getItem('token');
-    
+
     axios
     .delete(`http://localhost:3000/expense/delete/${expId}`,{ headers : {"Authorization" : token}})
     .then(()=>{
