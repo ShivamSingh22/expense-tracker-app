7fde9208bb4da631e1e238b6add18c3ff0500ca3
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-08-30 19:56:11 +0530

    premium UI

--- public/expense/expense.js
+++ public/expense/expense.js
@@ -164,3 +164,23 @@ function handleFormSubmit(event){
         console.log(err);
     })
 }
+
+function download(){
+    axios.get('http://localhost:3000/user/download', { headers: {"Authorization" : token} })
+    .then((response) => {
+        if(response.status === 201){
+            //the bcakend is essentially sending a download link
+            //  which if we open in browser, the file would download
+            var a = document.createElement("a");
+            a.href = response.data.fileUrl;
+            a.download = 'myexpense.csv';
+            a.click();
+        } else {
+            throw new Error(response.data.message)
+        }
+
+    })
+    .catch((err) => {
+        showError(err)
+    });
+}
