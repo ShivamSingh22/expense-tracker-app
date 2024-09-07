eaa7bf4b2ad19c57b42480601c3f5b74c8889ffc
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-09-07 21:02:03 +0530

    modified util

--- util/database.js
+++ util/database.js
@@ -3,10 +3,11 @@ const Sequelize = require('sequelize');
 const SQL_DB_NAME = process.env.SQL_DB_NAME;
 const SQL_USER_NAME = process.env.SQL_USER_NAME;
 const SQL_USER_PASS = process.env.SQL_USER_PASS;
+const DB_HOST = process.env.DB_HOST;
 
 const sequelize = new Sequelize(SQL_DB_NAME,SQL_USER_NAME,SQL_USER_PASS, {
     dialect: 'mysql',
-    host: 'localhost',
+    host: DB_HOST,
 });
 
 module.exports = sequelize;
